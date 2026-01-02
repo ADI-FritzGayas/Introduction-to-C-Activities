3993521521deb961a7f6d4e3f3505aae425bee8f
Fritz Gayas <251574282+ADI-FritzGayas@users.noreply.github.com>
2026-01-02 16:50:36 +0800

    Upload Solution for Activity A

---
+++ Activity A Files/Activity A - Solution.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    
+    int arrLib[] = {20, -20, 50, -50, 100};  
+    int arrLibSize = sizeof(arrLib)/sizeof(arrLib[0]); // determine the number of elements in arrLib
+    int i, j;
+
+    // Sorting
+    for (i = 0; i < arrLibSize - 1; i++) {
+        int swapped = 0;
+        // Compare first two elements
+        for (j = 0; j < arrLibSize - 1; j++) {
+            // For ascending order, swap if left > right
+            if (arrLib[j] > arrLib[j+1]) {
+                int temp = arrLib[j];
+                arrLib[j] = arrLib[j+1];
+                arrLib[j+1] = temp;
+                swapped = 1;
+            }
+        }
+    }
+
+    // Printing
+    printf("Sorted in Ascending Order: ");
+    for (i = 0; i < arrLibSize; i++) {
+        printf("%d ", arrLib[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
